3ccdb533cf31eaa02b8b3eaa423b6b2a0d038e00
Hyunwoo Shim <hkhunk0202@icloud.com>
2025-04-09 14:39:26 +0900

    [Bronze V] Title: 직사각형, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/27323. 직사각형/직사각형.cc
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+
+using namespace std;
+
+typedef long long ll;
+
+void solution()
+{
+	int a, b;
+	cin >> a >> b;
+
+	cout << a * b << endl;
+}
+
+int main(void)
+{
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+
+	solution();
+
+	return 0;
+}
