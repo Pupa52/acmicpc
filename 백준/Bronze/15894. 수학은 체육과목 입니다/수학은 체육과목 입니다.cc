5f64048b893c3cfde3fad2cb2b75e2fd0469b892
Hyunwoo Shim <hkhunk0202@icloud.com>
2025-04-10 14:30:57 +0900

    [Bronze III] Title: 수학은 체육과목 입니다, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/15894. 수학은 체육과목 입니다/수학은 체육과목 입니다.cc
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+using pii = pair<int, int>;
+
+void solution()
+{
+	unsigned int n;
+	cin >> n;
+
+	cout << n * 4 << endl;
+}
+
+int main(void)
+{
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+
+	solution();
+
+	return 0;
+}
