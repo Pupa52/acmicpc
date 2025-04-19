e8fbf23d1d4cbf02e69da9fc65ad5e20f995b522
Hyunwoo Shim <hkhunk0202@icloud.com>
2025-04-19 17:03:09 +0900

    [Bronze V] Title: A+B - 9, Time: 0 ms, Memory: 2020 KB, Score: 40 point -BaekjoonHub

---
+++ 백준/Bronze/15740. A＋B － 9/A＋B － 9.cc
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+
+void solution()
+{
+	int a, b;
+	cin >> a >> b;
+
+	cout << a + b << endl;
+}
+
+int main(void)
+{
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+
+	solution();
+
+	return 0;
+}
