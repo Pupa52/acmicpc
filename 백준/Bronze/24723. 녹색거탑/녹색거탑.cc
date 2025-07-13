2cbbcb59fe9fa97e38a28ae456c7ab80e0ad9638
Hyunwoo Shim <hkhunk0202@icloud.com>
2025-07-13 13:46:32 +0900

    [Bronze IV] Title: 녹색거탑, Time: 0 ms, Memory: 2208 KB -BaekjoonHub

---
+++ 백준/Bronze/24723. 녹색거탑/녹색거탑.cc
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <vector>
+#include <cmath>
+
+using namespace std;
+
+void solution()
+{
+	int n;
+	cin >> n;
+
+	cout << pow(2, n);
+}
+
+int main(void)
+{
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	solution();
+
+	return 0;
+}
