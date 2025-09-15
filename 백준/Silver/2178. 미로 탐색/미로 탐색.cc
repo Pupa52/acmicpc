a301bb8aff5733775c9dd4451de0816d5fd3905e
Hyunwoo Shim <hkhunk0202@icloud.com>
2025-09-15 19:58:08 +0900

    [Silver I] Title: 미로 탐색, Time: 0 ms, Memory: 2172 KB -BaekjoonHub

--- 백준/Silver/2178. 미로 탐색/미로 탐색.cc
+++ 백준/Silver/2178. 미로 탐색/미로 탐색.cc
@@ -1,84 +1,60 @@
 #include <iostream>
-#include <string>
-
 #include <vector>
+#include <string>
 #include <queue>
-#include <stack>
-#include <map>
-#include <set>
-
-#include <functional>
-#include <algorithm>
-#include <memory>
-
-#include <cstring>
 
 using namespace std;
 
-pair<int, int> offsets[4] = {
-	{-1,  0 },
-	{ 1,  0},
-	{ 0, -1},
-	{ 0,  1}
-};
-
-bool InBound(int row, int col, int width, int height)
-{
-	return (0 <= row && row < height) && (0 <= col && col < width);
-}
+int dx[] = { 1, 0, -1, 0 };
+int dy[] = { 0, 1, 0, -1 };
 
 void solution()
 {
 	int n{}, m{};
 	cin >> n >> m;
 
+	vector<string> graph(n);
+	for (auto& str : graph)
+		cin >> str;
+
+	vector<int> visit(n * m, 0);
 	queue<int> q;
-	vector<int> maze, visit;
-	maze.resize(n * m, 0);
-	visit.resize(n * m, 0);
 
-	for (int i = 0; i < n; i++)
-	{
-		char input[101] = {};
-		cin >> input;
+	visit[0] = 1;
+	q.push(0);
 
-		for (int j = 0; j < m; j++)
-			maze[i * m + j] = input[j] - '0';
-	}
-
-	q.push(0); // (1,1)
 	while (false == q.empty())
 	{
-		int index = q.front(); q.pop();
-
-		if (index == maze.size() - 1)
-			break;
-		if (1 == visit[index])
-			continue;
-
-		visit[index] = 1;
+		int curr = q.front(); q.pop();
 
 		for (int i = 0; i < 4; i++)
 		{
-			int row = index / m + offsets[i].first;
-			int col = index % m + offsets[i].second;
-			int next = row * m + col;
+			int x = curr % m + dx[i];
+			int y = curr / m + dy[i];
 
-			if (false == InBound(row, col, m, n) || 0 == maze[next])
+			if (!(0 <= x && x < m && 0 <= y && y < n))
 				continue;
 
-			maze[next] = maze[index] + 1;
+			if (graph[y][x] == '0')
+				continue;
+
+			int next = y * m + x;
+			if (visit[next] != 0)
+				continue;
+
+			visit[next] = visit[curr] + 1;
 			q.push(next);
 		}
 	}
 
-	cout << maze.back() << '\n';
+	cout << visit.back();
 }
 
 int main(void)
 {
 	ios::sync_with_stdio(false);
 	cin.tie(NULL);
+	cout.tie(NULL);
 
 	solution();
 
